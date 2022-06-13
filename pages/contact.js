c68ded2f005d1d6ea4fa25457a78964f904f2e24
fireteam99 <raymancheungsy@gmail.com>
2022-06-13 00:32:28 -0400

    Added some form field validation

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,5 @@
 import PageWrapper from '../components/PageWrapper';
+import { css } from '@emotion/react';
 import {
   FormControl,
   FormLabel,
@@ -10,43 +11,109 @@ import {
   HStack,
   Icon,
   Tooltip,
+  Button,
+  Alert,
+  AlertTitle,
+  AlertDescription,
+  AlertIcon,
+  UnorderedList,
+  ListItem,
 } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
 import { socials } from '../data/contact';
 
 export default function Contact() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setError,
+  } = useForm();
+  console.log(errors.message);
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <PageWrapper>
       <VStack w="100%">
         <VStack w="95%" maxW="80em" alignItems="flex-start" spacing="2em">
-          <Heading size="3xl">
-            Contact
-          </Heading>
-          <FormControl w="100%" maxW="60em">
-            <VStack alignItems="start">
-              <Box w="100%">
-                <FormLabel htmlFor="name">Name</FormLabel>
-                <Input id="name" color="white" />
-              </Box>
-              <Box w="100%">
-                <FormLabel htmlFor="email">Email</FormLabel>
-                <Input id="email" type="email" />
-              </Box>
-              <Box w="100%">
-                <FormLabel htmlFor="subject">Subject</FormLabel>
-                <Input id="subject" />
-              </Box>
-              <Box w="100%">
-                <FormLabel htmlFor="message">Message</FormLabel>
-                <Textarea id="message" height="7em" />
-              </Box>
-            </VStack>
-          </FormControl>
+          <Heading size="3xl">Contact</Heading>
+          <form
+            css={css`
+              width: 100%;
+              max-width: 60em;
+            `}
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <FormControl>
+             {Object.keys(errors).length > 0 && <Alert status="error">
+                <AlertIcon />
+                <Box>
+                <AlertTitle>
+                  Error
+                </AlertTitle>
+                <AlertDescription>
+                  <UnorderedList>
+                    {Object.keys(errors).map((key) => (
+                      <ListItem key={key}>{errors[key].message}</ListItem>
+                    ))}
+                  </UnorderedList>
+                </AlertDescription>
+
+                </Box>
+              </Alert>}
+              <VStack alignItems="start">
+                <Box w="100%">
+                  <FormLabel htmlFor="name">Name</FormLabel>
+                  <Input
+                    id="name"
+                    color="white"
+                    {...register('name', { required: 'Name is required' })}
+                  />
+                </Box>
+                <Box w="100%">
+                  <FormLabel htmlFor="email">Email</FormLabel>
+                  <Input
+                    id="email"
+                    type="email"
+                    {...register('email', { required: 'Email is required' })}
+                  />
+                </Box>
+                <Box w="100%">
+                  <FormLabel htmlFor="subject">Subject</FormLabel>
+                  <Input
+                    id="subject"
+                    {...register('subject', {
+                      required: 'Subject is required',
+                    })}
+                  />
+                </Box>
+                <Box w="100%">
+                  <FormLabel htmlFor="message">Message</FormLabel>
+                  <Textarea
+                    id="message"
+                    height="7em"
+                    {...register('message', {
+                      required: 'Message is required',
+                    })}
+                  />
+                </Box>
+                <Button type="submit" style={{ marginTop: '1em' }}>
+                  Submit
+                </Button>
+              </VStack>
+            </FormControl>
+          </form>
           <HStack>
             {socials.map(({ name, url, icon }) => (
-              <Tooltip label={name} fontSize='md' key={name + url}>
-              <a href={url}>
-                <Icon as={icon} color="light" boxSize="2em" />
-              </a>
+              <Tooltip label={name} fontSize="md" key={name + url}>
+                <a href={url}>
+                  <Icon as={icon} color="light" boxSize="2em" />
+                </a>
               </Tooltip>
             ))}
           </HStack>
